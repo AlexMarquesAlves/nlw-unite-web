a422d5ffc4924005b478b66ee693da0e66dbd2c6
¡Duque <atanaelleonardo@gmail.com>
2024-04-02 13:18:59 -0300

    feat: Update Search icon in AttendeeList component
    The Search icon in the AttendeeList component has been updated with a new prop `className` and the Search component is now passed the `size-4 text-emerald-300` props.

--- src/components/AttendeeList/index.tsx
+++ src/components/AttendeeList/index.tsx
@@ -11,7 +11,7 @@ export function AttendeeList({ children }: AttendeeListProps) {
       <div className="flex items-center gap-3">
         <h1 className="text-2xl font-bold">Participantes</h1>
         <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
-          <Search />
+          <Search className="size-4 text-emerald-300" />
           <input
             className="flex-1 bg-transparent outline-none"
             placeholder="Buscar participantes..."
