7e146b93a315459647742f3cd845cea77505eb27
¡Duque <atanaelleonardo@gmail.com>
2024-04-02 01:32:16 -0300

    feat: add header structure and styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,25 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
+import nlwUniteIcon from "../../assets/nlw-unite-icon.svg";
 
 interface HeaderProps {
-  children: ReactNode;
+  children?: ReactNode;
 }
 
 export function Header({ children }: HeaderProps) {
   return (
     <>
-      <h1>Header</h1>
+      <div className="flex items-center gap-5 py-2">
+        <img src={nlwUniteIcon} alt="" />
+
+        <nav className="flex items-center gap-5">
+          <a href="" className="font-medium text-sm text-zinc-300">
+            Eventos
+          </a>
+          <a href="" className="font-medium text-sm">
+            Participantes
+          </a>
+        </nav>
+      </div>
       {children}
     </>
   );
