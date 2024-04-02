6b808ceaee0c0a2f7f2b10778a0e485e7b7bd256
¡Duque <atanaelleonardo@gmail.com>
2024-04-02 13:32:35 -0300

    feat: "Add table and table body for attendee list"
    This commit adds a table and table body to the AttendeeList component. The table includes a header with columns for a checkbox, code, participants, registration date, check-in date, and an empty column. The table body includes a single row with values filled in for each column. The commit also includes a tfoot element with information about the number of items being shown and the current page. The commit finishes by adding the children prop to the end of the returned JSX.

--- src/components/AttendeeList/index.tsx
+++ src/components/AttendeeList/index.tsx
@@ -7,7 +7,7 @@ interface AttendeeListProps {
 
 export function AttendeeList({ children }: AttendeeListProps) {
   return (
-    <>
+    <div>
       <div className="flex items-center gap-3">
         <h1 className="text-2xl font-bold">Participantes</h1>
         <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
@@ -17,8 +17,48 @@ export function AttendeeList({ children }: AttendeeListProps) {
             placeholder="Buscar participantes..."
           />
         </div>
-        {children}
       </div>
-    </>
+
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <input type="checkbox" name="" id="" />
+            </th>
+            <th>Código</th>
+            <th>Participantes</th>
+            <th>Data de inscrição</th>
+            <th>Data do check-in</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <input type="checkbox" name="" id="" />
+            </td>
+            <td>12383</td>
+            <td>
+              <div>
+                <span>Diego Schell Fernandes</span>
+                <span>diego@rocketseat.com.br</span>
+              </div>
+            </td>
+            <td>7 dias atrás</td>
+            <td>3 dias atrás</td>
+            <td></td>
+          </tr>
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <td colSpan={3}>Mostrando 10 de 228 itens</td>
+            <td colSpan={3}>Página 1 de 23</td>
+          </tr>
+        </tfoot>
+      </table>
+      {children}
+    </div>
   );
 }
