2b7398b02a35ed8c6711b25753a2b7d4dfc9d957
¡Duque <atanaelleonardo@gmail.com>
2024-04-03 21:28:42 -0300

    feat: "Use twMerge to combine classnames in IconButton component"
    This commit updates the IconButton component to use the twMerge function from tailwind-merge to combine classnames instead of manually concatenating them. This makes the code more concise and easier to read. The commit also adds a condition to set the opacity of the button when it is disabled.

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface IconButtonProps extends ComponentProps<"button"> {
   children: ReactNode;
@@ -9,11 +10,16 @@ export function IconButton({ transparent, ...props }: IconButtonProps) {
   return (
     <button
       {...props}
-      className={
-        transparent
-          ? "border rounded-md bg-black/20 border-white/10 p-1.5"
-          : "border rounded-md bg-white/10 border-white/10 p-1.5"
-      }
+      // className={
+      //   transparent
+      //     ? "border rounded-md bg-black/20 border-white/10 p-1.5"
+      //     : "border rounded-md bg-white/10 border-white/10 p-1.5"
+      // }
+      className={twMerge(
+        "border rounded-md border-white/10 p-1.5",
+        transparent ? "bg-black/20" : "bg-white/10",
+        props.disabled ? "opacity-50" : null
+      )}
     >
       {props.children}
     </button>
